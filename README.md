aaa654da63226239d5b0f5cda0834c86a6875c64
Amir Nourinia <amir.nouri.nia@gmail.com>
2025-08-30 22:23:56 +0200

    feat: Initialize Android project structure with native C++ support
    - Added main C++ source file for rendering and input handling.
    - Created MainActivity in Kotlin extending GameActivity for Android integration.
    - Introduced vector drawable resources for app icons.
    - Configured adaptive launcher icons for various screen densities.
    - Defined application themes and color resources.
    - Implemented backup and data extraction rules XML files.
    - Added unit test example for local testing.
    - Set up Gradle build configuration with Kotlin DSL.
    - Established dependency management and plugin repositories.

--- README.md
+++
@@ -1,1 +0,0 @@
-# blue-xrstream-android-client
---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.bluecpp.bluexrstream"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.bluecpp.bluexrstream"
+        minSdk = 34
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters.addAll(listOf("x86_64", "arm64-v8a"))
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    buildFeatures {
+        prefab = true
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "4.1.0"
+        }
+    }
+    ndkVersion = "29.0.13846066 rc3"
+    buildToolsVersion = "36.0.0"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
+}
+
+
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.games.activity)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
